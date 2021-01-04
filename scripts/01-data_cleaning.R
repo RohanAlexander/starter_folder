573193a5d38bbcb97e1a00a2ab1b0c619a669036
Rohan Alexander <RohanAlexander@users.noreply.github.com>
2021-01-03 19:08:59 -0500

    Add example literature and a example script

---
+++ scripts/01-data_cleaning.R
@@ -0,0 +1,36 @@
+#### Preamble ####
+# Purpose: Clean the survey data downloaded from [...UPDATE ME!!!!!]
+# Author: Rohan Alexander [CHANGE THIS TO YOUR NAME!!!!]
+# Data: 3 January 2021
+# Contact: rohan.alexander@utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
+# License: MIT
+# Pre-requisites: 
+# - Need to have downloaded the ACS data and saved it to inputs/data
+# - Don't forget to gitignore it!
+# - Change these to yours
+# Any other information needed?
+
+
+#### Workspace setup ####
+# Use R Projects, not setwd().
+library(haven)
+library(tidyverse)
+# Read in the raw data. 
+raw_data <- readr::read_csv("inputs/data/raw_data.csv"
+                     )
+# Just keep some variables that may be of interest (change 
+# this depending on your interests)
+names(raw_data)
+
+reduced_data <- 
+  raw_data %>% 
+  select(first_col, 
+         second_col)
+rm(raw_data)
+         
+
+#### What's next? ####
+
+
+
+         
