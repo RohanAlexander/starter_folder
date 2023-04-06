0abcb4a83aaaf30a90640be31d68d9506d6fcd2d
Rohan Alexander <RohanAlexander@users.noreply.github.com>
2023-04-06 16:49:02 -0400

    Update with a model example

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,26 +1,44 @@
 #### Preamble ####
-# Purpose: Cleans.... [...UPDATE THIS...]
+# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
 # Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
+# Date: 6 April 2023 [...UPDATE THIS...]
 # Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
 # Any other information needed? [...UPDATE THIS...]
 
-
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Clean data ####
-# [...UPDATE THIS...]
-raw_data <- read_csv("inputs/data/raw_data.csv")
+raw_data <- read_csv("inputs/data/plane_data.csv")
 
-
-
+cleaned_data <-
+  raw_data |>
+  janitor::clean_names() |>
+  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
+  filter(wing_width_mm != "caw") |>
+  mutate(
+    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
+                                   "1.35",
+                                   flying_time_sec_first_timer)
+  ) |>
+  mutate(wing_width_mm = if_else(wing_width_mm == "490",
+                                 "49",
+                                 wing_width_mm)) |>
+  mutate(wing_width_mm = if_else(wing_width_mm == "6",
+                                 "60",
+                                 wing_width_mm)) |>
+  mutate(
+    wing_width_mm = as.numeric(wing_width_mm),
+    wing_length_mm = as.numeric(wing_length_mm),
+    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
+  ) |>
+  rename(flying_time = flying_time_sec_first_timer,
+         width = wing_width_mm,
+         length = wing_length_mm
+         ) |> 
+  tidyr::drop_na()
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change cleaned_data to whatever name you end up with at the end of cleaning
-write_csv(cleaned_data, "outputs/data/cleaned_data.csv")
-
+write_csv(cleaned_data, "outputs/data/analysis_data.csv")
---
+++ scripts/04-model.R
@@ -0,0 +1,37 @@
+#### Preamble ####
+# Purpose: Models... [...UPDATE THIS...]
+# Author: Rohan Alexander [...UPDATE THIS...]
+# Date: 11 February 2023 [...UPDATE THIS...]
+# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# License: MIT
+# Pre-requisites: [...UPDATE THIS...]
+# Any other information needed? [...UPDATE THIS...]
+
+
+#### Workspace setup ####
+library(tidyverse)
+library(rstanarm)
+
+#### Read data ####
+analysis_data <- read_csv("outputs/data/analysis_data.csv")
+
+### Model data ####
+first_model <-
+  stan_glm(
+    formula = flying_time ~ length + width,
+    data = analysis_data,
+    family = gaussian(),
+    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    prior_aux = exponential(rate = 1, autoscale = TRUE),
+    seed = 853
+  )
+
+
+#### Save model ####
+saveRDS(
+  first_model,
+  file = "outputs/models/first_model.rds"
+)
+
+
