d14be06fb2f01cc1664edc014f730ccc43c075b5
Luca Carnegie <41924665+lcarnegie@users.noreply.github.com>
2024-07-16 13:09:28 -0400

    started cleaning data

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,44 +1,41 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Cleans the raw survey data recored by Rohan at the end of STA302
+# Author: Luca Carnegie
+# Date: 16 July 2024
+# Contact: luca.carnegie@mail.utoronto.ca or rohan.alexander@utoronto.ca
+# Pre-requisites: 01-download_data.R
 
 #### Workspace setup ####
 library(tidyverse)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+raw_data <- read_csv("data/raw_data/raw_STA302_postcourse_survey_w24.csv")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+raw_data <- as_tibble(raw_data)
+
+# Drop unnecessary columns
+
+raw_data <- raw_data |>
+  select(-c(Email, 
+            Name, 
+            `Last modified time`, 
+            `What is your full name on Quercus?`, #anonymized
+            `What is your Student ID?`, #anonymized 
+            `What is/are your major/s?`, #no answer for any 
+            `What is/are your minor/s?`, #no answer for any 
+            ))
+
+
+# Clean specialization column
+
+# Clean GPA column
+
+# Fix text errors in self-perception question
+
+# Replace erroneous characters with NA, etc. 
+
+# Fix "Any other comments?" column
+
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(cleaned_data, "data/analysis_data/clean_STA302_postcourse_survey_w24.csv")
