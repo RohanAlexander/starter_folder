42c7255d4f15a9c14a835ed1a36f56aa330c9bf1
Rohan Alexander <rohan.alexander@utoronto.ca>
2024-08-04 14:01:01 -0400

    First round

--- README.md
+++ README.md
@@ -1,28 +1,20 @@
-# *Working Paper*: Usage of LLM Tools and Student Performance
+# Self-reported LLM usage and results on a datascience project: Evidence from a Canadian undergraduate data science course
 
 ## Overview
 
-This repo contains the materials and work for a working paper studying the relationship between the usage of LLM tools like ChatGPT and student educational achievement, using data from undergraduate performance in an upper-level Statistics course as a key example.
+To help understand the effect of Large Language Models (LLMs) on data science practice we examine the extent to which LLM usage is correlated with the mark that a student gets on a final paper in a classroom data science setting. We find no clear relationship between more extensive LLM usage and the student’smark. Despite the classroom setting used for evaluation, the particular activity of interest reflects the work done by professional data scientists. Our finding suggests the need for more extensive work evaluating how LLMs can be integrated into the data science workflow in a way that provides value.
 
-
-## File Structure
+## File structure
 
 The repo is structured as:
 
--   `data/raw_data` contains the raw data as obtained from X.
+-   `data/raw_data` contains the raw data.
 -   `data/analysis_data` contains the cleaned dataset that was constructed.
--   `model` contains fitted models. 
+-   `model` contains fitted models.
 -   `other` contains relevant literature, details about LLM chat interactions, and sketches.
 -   `paper` contains the files used to generate the paper, including the Quarto document and reference bibliography file, as well as the PDF of the paper. 
 -   `scripts` contains the R scripts used to simulate, download and clean data.
 
-
 ## Statement on LLM usage
 
-Aspects of the code were written with the help of models like GitHub Copilot and GPT-4o. The [parts of the paper] were written with the help of Claude-3.5 Sonnet. The entire chat history with each particular model is available in inputs/llms/usage.txt.
-
-## Some checks
-
-- [ ] Remove files that you're not using
-- [ ] Update comments in R scripts
-- [ ] Remove this checklist
+Aspects of the code were written with the help of GitHub Copilot and GPT-4o. Aspects of the paper were written with the help of Claude-3.5 Sonnet. Rohan Alexander takes complete and final responsibility for the content of both code and text.
--- scripts/04-model.R
+++ scripts/04-model.R
@@ -1,37 +1,73 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Models
+# Author: Rohan Alexander
+# Date: 4 August 2024
+# Contact: rohan.alexander@utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: -
+# Any other information needed? -
 
 
 #### Workspace setup ####
 library(tidyverse)
 library(rstanarm)
+library(brms)
+library(readxl)
+
 
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+all_data <-
+  read_excel(here::here("data/raw_data/merged_data.xlsx"), sheet = "Sheet1") |>
+  janitor::clean_names() |>
+  mutate(mark = if_else(mark == 1, 0.99, mark)) |> # I'm a monster
+  mutate(llm_usage =
+           if_else(llm_usage %in% c("Minimal", "None"), "None or minimal", llm_usage)) |>
+  filter(!is.na(what_is_your_gpa))
+
 
 ### Model data ####
-first_model <-
-  stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
+# Using rstanarm
+fit1rstanarm <-
+  stan_betareg(mark ~ llm_usage,
+               data = all_data,
+               link = "logit",
+               seed = 853)
+
+fit2rstanarm <-
+  stan_betareg(
+    mark ~ llm_usage + what_is_your_gpa,
+    data = all_data,
+    link = "logit",
     seed = 853
   )
 
+# Using brms
+fit1brms <- brm(
+  data = all_data,
+  family = Beta,
+  mark ~ llm_usage,
+  prior = prior(normal(0, 1), class = b) +
+    prior(gamma(4, 0.1), class = phi),
+  cores = 4,
+  seed = 853
+)
 
-#### Save model ####
-saveRDS(
-  first_model,
-  file = "models/first_model.rds"
+fit2brms <- brm(
+  data = all_data,
+  family = Beta,
+  mark ~ llm_usage + what_is_your_gpa,
+  prior = prior(normal(0, 1), class = b) +
+    prior(gamma(4, 0.1), class = phi),
+  cores = 4,
+  seed = 853
 )
 
 
+#### Save model ####
+saveRDS(fit1rstanarm, file = "models/fit1rstanarm.rds")
+
+saveRDS(fit2rstanarm, file = "models/fit2rstanarm.rds")
+
+saveRDS(fit1brms, file = "models/fit1brms.rds")
+
+saveRDS(fit2brms, file = "models/fit2brms.rds")
