6d980131caae5c3d37cf36a5516afc939f21009c
dennisnetchitailo <156948879+dennisnetchitailo@users.noreply.github.com>
2024-09-22 19:28:36 -0400

    Update 00-simulate_data.R
    Changed some "Update this".

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,11 +1,15 @@
 #### Preamble ####
 # Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: Dennis Netchitailo
+# Date: 23 September 2024 
+# Contact: dennis.netchitailo@utoronto.ca 
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
 # Any other information needed? [...UPDATE THIS...]
+
+
+
+
 
 
 #### Workspace setup ####
