9f5a72baf5807ca01774f5d28b2b0f4d42c9e3b2
younazhao <youna.zhao@mail.utoronto.ca>
2024-10-22 15:09:41 -0400

    Update 02-download_data.R

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,26 +1,24 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from 538 president general 
+# Authors: Yun Chu, Felix Li, and Wen Han Zhao 
+# Date: 22 October 2024
+# Contact: youna.zhao@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Dataset found from 538 website
+# Any other information needed? None
 
 
 #### Workspace setup ####
-library(opendatatoronto)
+
 library(tidyverse)
-# [...UPDATE THIS...]
+library(readr)
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
 
+president_polls <- read_csv("~/Downloads/president_polls.csv")
+data <- president_polls
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
 
-         
+write_csv(data, "data/01-raw_data/raw_data.csv") 
