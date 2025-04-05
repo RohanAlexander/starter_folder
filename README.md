1370e925c4a2cc149c4663dd784a1f3f1749a4d6
Rohan Alexander <rohan.alexander@utoronto.ca>
2025-04-05 06:24:04 -0400

    Add SSTA304

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
-# Self-reported LLM Usage and Outcomes on a Data Science Project: Evidence from a Canadian Undergraduate Data Science Course
+# Self-reported LLM usage and outcomes on a data science project: Evidence from two undergraduate data science courses
 
 ## Overview
 
-To help understand the effect of Large Language Models (LLMs) on data science practice we examine the extent to which LLM usage is correlated with the mark that a student gets on a final paper in a classroom data science setting. We find some very mild evidence from this observational study that LLM usage may be associated with better scores, but our main conclusion is that there is no clear relationship between more extensive LLM usage and the student's mark. Despite the classroom setting used for evaluation, the particular activity of interest reflects the work done by professional data scientists. Our finding suggests the need for more extensive work evaluating how LLMs can be integrated into the data science workflow in a way that provides value.
+To help understand the effect of Large Language Models (LLMs) on data science practice we examine the extent to which self-reported LLM usage is correlated with the mark that a student gets on a final paper in a classroom data science setting. We find some very mild evidence from this observational study that LLM usage may be associated with better scores, but our main conclusion is that there is no clear relationship between more extensive LLM usage and the student's mark. Despite the classroom setting used for evaluation, the task of interest is similar to the work done by professional data scientists. Comparing self-reported usage, there was a considerable increase between the class in January-April 2024 where 41 per cent of students self-reported extensive LLM usage and the class in September-December 2024 where 69 per cent reported extensive LLM usage. Our finding suggests the need for more extensive work evaluating how LLMs can be integrated into the data science workflow in a way that provides value.
 
 ## File structure
 
 The repo is structured as:
 
 -   `data` contains the data.
--   `model` contains fitted models.
+-   `models` contains fitted models.
 -   `other` contains relevant literature and a copy of the survey questions.
--   `paper` contains the files used to generate the paper, including the Quarto document and reference bibliography file, as well as the PDF of the paper. 
+-   `paper` contains the files used to generate the paper, including the Quarto and bibtex files, as well as the PDF of the paper. 
 -   `scripts` contains the R scripts used to simulate and model the data.
 
 ## Statement on LLM usage
 
-Aspects of the code were written with the help of GitHub Copilot and GPT-4o. Aspects of the paper were written with the help of Claude-3.5 Sonnet.
+Aspects of the code were written with the help of GitHub Copilot and GPT-4o.
--- scripts/03-model.R
+++ scripts/03-model.R
@@ -1,7 +1,7 @@
 #### Preamble ####
 # Purpose: Models
 # Author: Rohan Alexander
-# Date: 4 August 2024
+# Date: 17 March 2025
 # Contact: rohan.alexander@utoronto.ca
 # License: MIT
 # Pre-requisites: -
@@ -21,8 +21,8 @@ all_data <-
 
 
 # Change the reference level so it's in terms of none
-all_data <- 
-  all_data |> 
+all_data <-
+  all_data |>
   mutate(llm_usage = relevel(llm_usage, ref = "None or minimal"))
 
 
@@ -35,10 +35,12 @@ all_data <-
 
 # Using rstanarm
 fit1rstanarm <-
-  stan_betareg(mark_no_ones ~ llm_usage,
-               data = all_data,
-               link = "logit",
-               seed = 853)
+  stan_betareg(
+    mark_no_ones ~ llm_usage,
+    data = all_data,
+    link = "logit",
+    seed = 853
+  )
 
 fit2rstanarm <-
   stan_betareg(
@@ -76,12 +78,7 @@ fit2brms <- brm(
 # https://www.andrewheiss.com/blog/2021/11/08/beta-regression-guide/#special-case-2-one-inflated-beta-regression
 
 fit_one_inflated <- brm(
-  formula = bf(
-    mark ~ llm_usage,
-    phi ~ llm_usage,
-    zoi ~ llm_usage,
-    coi ~ 1
-  ),
+  formula = bf(mark ~ llm_usage, phi ~ llm_usage, zoi ~ llm_usage, coi ~ 1),
   family = zero_one_inflated_beta(),
   data = all_data,
   cores = 4,
@@ -114,3 +111,161 @@ saveRDS(fit2brms, file = "models/fit2brms.rds")
 saveRDS(fit_one_inflated, file = "models/fit_one_inflated.rds")
 
 saveRDS(fit_one_inflated_gpa, file = "models/fit_one_inflated_gpa.rds")
+
+
+
+
+
+#### Redo with STA304 ####
+#### Read data ####
+sta304_data <-
+  read_parquet(here("data/sta304_analysis_data_classified_cleaned.parquet"))
+
+# Change the reference level so it's in terms of none
+sta304_data <-
+  sta304_data |>
+  mutate(llm_usage = relevel(llm_usage, ref = "None or minimal"))
+
+
+#### Model data ####
+#### Simplified data ####
+# Change the 1s to 0.99 so we can just use Beta initially
+sta304_data <-
+  sta304_data |>
+  mutate(mark_no_ones = if_else(mark == 1, 0.99, mark))
+
+# Using rstanarm
+fit1rstanarm_sta304 <-
+  stan_betareg(
+    mark_no_ones ~ llm_usage,
+    data = sta304_data,
+    link = "logit",
+    seed = 853
+  )
+
+fit2rstanarm_sta304 <-
+  stan_betareg(
+    mark_no_ones ~ llm_usage + what_is_your_gpa,
+    data = sta304_data,
+    link = "logit",
+    seed = 853
+  )
+
+# Using brms
+fit1brms_sta304 <-
+  brm(
+    data = sta304_data,
+    family = Beta,
+    mark_no_ones ~ llm_usage,
+    prior = prior(normal(0, 1), class = b) +
+      prior(gamma(4, 0.1), class = phi),
+    cores = 4,
+    seed = 853
+  )
+
+fit2brms_sta304 <-
+  brm(
+    data = sta304_data,
+    family = Beta,
+    mark_no_ones ~ llm_usage + what_is_your_gpa,
+    prior = prior(normal(0, 1), class = b) +
+      prior(gamma(4, 0.1), class = phi),
+    cores = 4,
+    seed = 853
+  )
+
+
+#### Real data ####
+# There are no zeros, but there are ones so need one-inflated Beta
+# Achieve by using zero-one-inflated Beta and setting coi to 1 following:
+# https://www.andrewheiss.com/blog/2021/11/08/beta-regression-guide/#special-case-2-one-inflated-beta-regression
+
+fit_one_inflated_sta304 <-
+  brm(
+    formula = bf(mark ~ llm_usage, phi ~ llm_usage, zoi ~ llm_usage, coi ~ 1),
+    family = zero_one_inflated_beta(),
+    data = sta304_data,
+    cores = 4,
+    seed = 853
+  )
+
+fit_one_inflated_gpa_sta304 <- brm(
+  formula = bf(
+    mark ~ llm_usage + what_is_your_gpa,
+    phi ~ llm_usage + what_is_your_gpa,
+    zoi ~ llm_usage + what_is_your_gpa,
+    coi ~ 1
+  ),
+  family = zero_one_inflated_beta(),
+  data = sta304_data,
+  cores = 4,
+  seed = 853
+)
+
+## Add in whether they have ESL
+fit_one_inflated_gpa_english_sta304 <- brm(
+  formula = bf(
+    mark ~ llm_usage + what_is_your_gpa + native_english,
+    phi ~ llm_usage + what_is_your_gpa + native_english,
+    zoi ~ llm_usage + what_is_your_gpa + native_english,
+    coi ~ 1
+  ),
+  family = zero_one_inflated_beta(),
+  data = sta304_data,
+  cores = 4,
+  seed = 853
+)
+
+
+
+
+
+
+#### Combine both and then add a flag ####
+sta304_data_reduced <-
+  sta304_data |>
+  select(mark, llm_usage, what_is_your_gpa) |>
+  mutate(class = "sta304")
+
+sta302_data_reduced <-
+  all_data |>
+  select(mark, llm_usage, what_is_your_gpa) |>
+  mutate(class = "sta302")
+
+both <-
+  rbind(sta302_data_reduced, sta304_data_reduced)
+
+fit_one_inflated_gpa_both <- brm(
+  formula = bf(
+    mark ~ llm_usage + what_is_your_gpa + class,
+    phi ~ llm_usage + what_is_your_gpa + class,
+    zoi ~ llm_usage + what_is_your_gpa + class,
+    coi ~ 1
+  ),
+  family = zero_one_inflated_beta(),
+  data = both,
+  cores = 4,
+  seed = 853
+)
+
+
+
+
+
+
+#### Save models ####
+saveRDS(fit1rstanarm_sta304, file = "models/fit1rstanarm_sta304.rds")
+
+saveRDS(fit2rstanarm_sta304, file = "models/fit2rstanarm_sta304.rds")
+
+saveRDS(fit1brms_sta304, file = "models/fit1brms_sta304.rds")
+
+saveRDS(fit2brms_sta304, file = "models/fit2brms_sta304.rds")
+
+saveRDS(fit_one_inflated_sta304, file = "models/fit_one_inflated_sta304.rds")
+
+saveRDS(fit_one_inflated_gpa_sta304, file = "models/fit_one_inflated_gpa_sta304.rds")
+
+saveRDS(fit_one_inflated_gpa_english_sta304, file = "models/fit_one_inflated_gpa_english_sta304.rds")
+
+saveRDS(fit_one_inflated_gpa_both, file = "models/fit_one_inflated_gpa_both.rds")
